50233d5c6e3989fe4f01a56e71fdebbdaa46309d
RubikCubed <spider@tilde.cafe>
2024-09-30 10:59:03 +1000

    update lockfile

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
 
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/cf8b6e2d4e8aca8ef14b839a906ab5eb98b08561";
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
