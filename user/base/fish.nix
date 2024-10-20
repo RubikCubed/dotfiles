fe7e9d31609ff2a4439ca7516cc59fe5d63e02cf
RubikCubed <spider@tilde.cafe>
2024-10-21 10:53:17 +1100

    add fish shortcuts

--- user/base/fish.nix
+++ user/base/fish.nix
@@ -4,16 +4,17 @@ _: {
     interactiveShellInit = ''
       set fish_greeting
       bind -k nul accept-autosuggestion
+      bind \e\[1\;3A 'cd ..; fish_prompt'
     '';
-    #shellAbbrs = {
-    #  nr = {
-    #    expansion = "nix run nixpkgs#%";
-    #    setCursor = true;
-    #  };
-    #  ns = {
-    #    expansion = "nix shell nixpkgs#%";
-    #    setCursor = true;
-    #  };
-    #};
+    shellAbbrs = {
+      nr = {
+        expansion = "nix run nixpkgs#%";
+        setCursor = true;
+      };
+      ns = {
+        expansion = "nix shell nixpkgs#%";
+        setCursor = true;
+      };
+    };
   };
 }
