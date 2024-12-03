3edd5406a8f4424083cbcda3b164360f14f7fd84
RubikCubed <spider@tilde.cafe>
2024-12-04 09:18:17 +1100

    update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "oldmate's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
     stylix.url = "github:danth/stylix/cf8b6e2d4e8aca8ef14b839a906ab5eb98b08561";
     helix.url = "github:helix-editor/helix";
@@ -18,7 +18,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
